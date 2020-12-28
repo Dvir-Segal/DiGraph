73073d4da6172f8d9c34634e5673fa2fd16fa862
shaCode256 <61514960+shaCode256@users.noreply.github.com>
2020-12-29 01:07:53 +0200

    Add files via upload

--- tests/testNodeData.py
+++ tests/testNodeData.py
@@ -19,7 +19,14 @@ class MyTestCase(unittest.TestCase):
 
     def test_setAndGetKeysAndInfos(self):
         """
-        Test that we can create a new NodeData. and get it's key (in the beginning should be 0).
+        Test that we can create Node Datas, set and get their infos,
+         and get their keys(in the beginning should be 0).
+         Then, to CHANGE their keys and infos, and get them changed:
+         *create Node Datas
+         *get their keys
+         *set and get their infos
+         *set (change) and get their keys
+         *set (change) and get their infos
         """
         # creating a new node
         node1 = NodeData()
@@ -34,6 +41,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual("first", node1.getInfo())
         print("key of node2 is", node2.getInfo())
         self.assertEqual("second", node2.getInfo())
+        print("set the info of node1 to be change1", node1.setInfo("change1"))
+        self.assertEqual("change1", node1.getInfo())
+        print("change the key of node2 to be 3", node2.setkey(3))
+        self.assertEqual(3, node2.getKey())
 
 
 if __name__ == '__main__':
